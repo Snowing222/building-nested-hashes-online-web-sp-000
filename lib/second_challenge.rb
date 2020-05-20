3ba6540ce87c89d3b1692c164323eb48e6517bb6
elsa chen <snowing222@gmail.com>
2020-05-20 12:52:47 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,9 +1,17 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
+      Patriarch:{},
+      Matriarch:{},
+      Hero:{},
+      hero_friends:{}
       #your key/value pairs here!
       }, 
     :capulet => {
+      Patriarch:{},
+      Matriarch:{},
+      heroine:{},
+      heroine_friends:{}
       #your key/value pairs here!
       }}
 
